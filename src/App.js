a69cc48e61611eb3467051d8261060bd53fdddfb
Yomirose <roselinefayomi10@yahoo.comcleargit config --global user.email roselinefayomi10@yahoo.com>
2024-08-05 14:22:17 +0100

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import MovieList from './components/MovieList';
+import Filter from './components/Filter';
+import AddMovie from './components/AddMovie';
 import './App.css';
 
 function App() {
+  const [movies, setMovies] = useState([
+    {
+      title: 'Inception',
+      description: 'A thief who steals corporate secrets through the use of dream-sharing technology.',
+      posterURL: 'https://youtu.be/LifqWf0BAOA?si=pc5XSuBdKdHPcoc1',
+      rating: 8.8
+    },
+  ]);
+
+  const handleAddMovie = (movie) => {
+    setMovies([...movies, movie]);
+  };
+
+  const handleFilter = (filter) => {
+    const { title, rating } = filter;
+    let filteredMovies = movies;
+
+    if (title) {
+      filteredMovies = filteredMovies.filter(movie => 
+        movie.title.toLowerCase().includes(title.toLowerCase())
+      );
+    }
+
+    if (rating) {
+      filteredMovies = filteredMovies.filter(movie => 
+        movie.rating >= rating
+      );
+    }
+
+    setMovies(filteredMovies);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>My Movie App</h1>
+      <Filter onFilter={handleFilter} />
+      <AddMovie onAdd={handleAddMovie} />
+      <MovieList movies={movies} />
     </div>
   );
 }
